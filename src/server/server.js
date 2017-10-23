33f18b9eda2712845697b08ba927621c4d98ae29
Matt Cole <matt@matt-cole.co.uk>
2017-10-23 22:47:59 +0100

    Allow GZip compression

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import path from 'path';
+import compression from 'compression';
 import api from './routes';
 import renderApp from './universal';
 
 const app = express();
+
+if (process.env.GZIP === 'true') {
+  app.use(compression());
+}
 
 app.use(express.static(path.resolve(__dirname, 'public'), { index: false }));
 app.use('/api', api());
